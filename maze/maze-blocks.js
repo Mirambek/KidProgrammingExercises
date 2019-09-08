edf46dcefe7a89cfcd6909379fd60f7a47da0481
MirambekNagashbekov <n.mirambek@gmail.com>
2019-09-08 11:28:48 +0600

    refactoring

--- maze/maze-blocks.js
+++ maze/maze-blocks.js
@@ -1,23 +1,38 @@
+const CURRENT_GAME=window['CURRENT_GAME'];
 let Directions={
    MOVE_FORWARD:"MOVE_FORWARD",
    TURN_LEFT:"TURN_LEFT",
    TURN_RIGHT:"TURN_RIGHT"
 };
-let actorStyle=document.getElementById("actor").style;
-
-let RightPath=[{Direction: Directions.MOVE_FORWARD, Left:"305px"}, {Direction: Directions.TURN_LEFT,Top:"109px"}];
-let RightPathCopy= [];
-for(let i=0;i<RightPath.length;i++){
-    RightPathCopy.push(RightPath[i]);
+let actorStyle=document.getElementById("GAME-" + window.CURRENT_GAME).getElementsByClassName("actor")[0].style;
+let MazeGames=[
+    {
+        RightPath:[{Direction: Directions.MOVE_FORWARD, Left:"305px"}, 
+        {Direction: Directions.TURN_LEFT,Top:"109px"}],
+        InitialActorCoordinates:{X:"200px",Y:"70px"}
+    },
+    {
+        RightPath:[{Direction: Directions.MOVE_FORWARD, Left:"305px"}, {Direction: Directions.TURN_LEFT,Top:"109px"}],
+        InitialActorCoordinates:{X:"200px",Y:"70px"}
+    },
+    {
+        RightPath:[{Direction: Directions.MOVE_FORWARD, Left:"305px"}, {Direction: Directions.TURN_LEFT,Top:"109px"}],
+        InitialActorCoordinates:{X:"200px",Y:"70px"}
+    },
+];
+let makeCopy=function(m){
+       return { RightPath:m.RightPath.map(m=>m),
+        InitialActorCoordinates:m.InitialActorCoordinates};
 }
+let MazeGamesCopy= MazeGames.map(makeCopy);
+
 function reset(){
-    alert(1);
-    RightPath=RightPathCopy.map(m=>m);
-    actorStyle.top="200px";
-    actorStyle.left= "70px";
+    MazeGames=MazeGamesCopy.map(makeCopy);
+    actorStyle.top=MazeGames[CURRENT_GAME].InitialActorCoordinates.Y;
+    actorStyle.left=MazeGames[CURRENT_GAME].InitialActorCoordinates.X;
 }
 let move=function (currentMoveMent){
-    alert(2);
+    alert();
     if (currentMoveMent.Top){
         actorStyle.top=currentMoveMent.Top;
     }
@@ -40,7 +55,7 @@ Blockly.Blocks[Directions.MOVE_FORWARD] = {
   Blockly.Blocks[Directions.TURN_LEFT] = {
     init: function() {
       this.appendDummyInput()
-          .appendField("Алдыға");
+          .appendField("Солға");
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
       this.setColour(270);
@@ -52,7 +67,7 @@ Blockly.Blocks[Directions.MOVE_FORWARD] = {
   Blockly.Blocks[Directions.TURN_RIGHT] = {
     init: function() {
       this.appendDummyInput()
-          .appendField("Алдыға");
+          .appendField("Оңға");
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
       this.setColour(270);
@@ -63,12 +78,14 @@ Blockly.Blocks[Directions.MOVE_FORWARD] = {
   };
   function runBlockCode(block) {
     var code='';
-    let currentMovement=RightPath.shift();
+    console.log(CURRENT_GAME);
+    console.log(MazeGames[CURRENT_GAME]);
+    let currentMovement=MazeGames[CURRENT_GAME].RightPath.shift();
     if (block.Direction===currentMovement.Direction){
         move(currentMovement);
     } 
     else {
-        reset();
+        code="result=false;";        
     }
     return code;
   };
